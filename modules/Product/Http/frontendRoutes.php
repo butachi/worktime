036859333f63c62223f7c95e9122f9216f7c803d
butachi <butachi@live.com>
2015-10-12 16:33:31 +0700

    update module user

--- modules/Product/Http/frontendRoutes.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Routing\Router;
-
-/** @var Router $router */
-if (! App::runningInConsole()) {
-    $router->get('{uri}', ['uses' => 'PublicController@uri', 'as' => 'page']);
-    $router->get('/', ['uses' => 'PublicController@homepage', 'as' => 'homepage']);
-}
--- modules/Product/composers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-view()->composer(['page::admin.create', 'page::admin.edit'], 'Modules\Page\Composers\TemplateViewComposer');
