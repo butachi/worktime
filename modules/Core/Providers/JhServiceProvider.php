1732bb98c9960cbe6e66094f6d3639ad151e831b
butachi <hungtk49@yahoo.com>
2015-10-12 23:54:36 +0700

    commit code lan 1

---
+++ modules/Core/Providers/JhServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Modules\Core\Providers;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+
+class JhServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        $this->app->register('Pingpong\Modules\ModulesServiceProvider');
+
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Module', 'Pingpong\Modules\Facades\Module');
+	}
+}
